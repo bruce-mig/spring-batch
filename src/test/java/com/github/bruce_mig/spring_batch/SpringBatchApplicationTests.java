4fb77451e6f958490c8de9f5cfd3bad8a43de293
bruce-mig <brucetmig@icloud.com>
2024-10-19 15:09:28 +0200

    first commit

---
+++ src/test/java/com/github/bruce_mig/spring_batch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
