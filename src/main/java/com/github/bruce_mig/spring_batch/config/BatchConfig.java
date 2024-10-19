05df445ed6754d78b39b0999b7febba92034edb0
bruce-mig <brucetmig@icloud.com>
2024-10-19 16:21:51 +0200

    added batch config class

---
+++ src/main/java/com/github/bruce_mig/spring_batch/config/BatchConfig.java
@@ -0,0 +1,88 @@
+package com.github.bruce_mig.spring_batch.config;
+
+import com.github.bruce_mig.spring_batch.student.Student;
+import com.github.bruce_mig.spring_batch.student.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class BatchConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final StudentRepository repository;
+
+    @Bean
+    public FlatFileItemReader<Student> itemReader(){
+        FlatFileItemReader<Student> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/students.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+        return itemReader;
+    }
+
+    @Bean
+    public StudentProcessor processor(){
+        return new StudentProcessor();
+    }
+
+
+    @Bean
+    public RepositoryItemWriter<Student> writer(){
+        RepositoryItemWriter<Student> writer = new RepositoryItemWriter<>();
+        writer.setRepository(repository);
+        writer.setMethodName("save");
+        return  writer;
+    }
+
+    @Bean
+    public Step importStep(){
+        return new StepBuilder("csvImport", jobRepository)
+                .<Student, Student>chunk(10, platformTransactionManager)
+                .reader(itemReader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job runJob(){
+        return new JobBuilder("importStudents", jobRepository)
+                .start(importStep())
+//                .next() // for subsequent steps
+                .build();
+    }
+
+    private LineMapper<Student> lineMapper(){
+        DefaultLineMapper<Student> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("id","firstname", "lastname", "age");
+
+        BeanWrapperFieldSetMapper<Student> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Student.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return lineMapper;
+    }
+
+}
