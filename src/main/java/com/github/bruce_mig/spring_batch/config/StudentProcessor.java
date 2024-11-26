c4266627603470c01af5126be462b7153d7d3912
bruce-mig <brucetmig@icloud.com>
2024-11-26 15:23:53 +0200

    made StudentProcessor class into a component

--- src/main/java/com/github/bruce_mig/spring_batch/config/StudentProcessor.java
+++ src/main/java/com/github/bruce_mig/spring_batch/config/StudentProcessor.java
@@ -1,14 +1,19 @@
 package com.github.bruce_mig.spring_batch.config;
 
 import com.github.bruce_mig.spring_batch.student.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class StudentProcessor implements ItemProcessor<Student,Student> {
 
     @Override
     public Student process(Student student) {
         // all the business logic goes here
         student.setId(null);
+        log.info("processing the item: {}", student);
         return student;
     }
 }
