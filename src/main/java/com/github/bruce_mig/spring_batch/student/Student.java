971128c28bc02725c1086be60b15566e5107f343
bruce-mig <brucetmig@icloud.com>
2024-11-27 21:26:08 +0200

    added to string annotation

--- src/main/java/com/github/bruce_mig/spring_batch/student/Student.java
+++ src/main/java/com/github/bruce_mig/spring_batch/student/Student.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Entity
+@ToString
 public class Student {
 
     @Id
