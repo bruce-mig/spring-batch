bfc66e75856fd6d00eae853d6b4c7c705f0be5ae
bruce-mig <brucetmig@icloud.com>
2024-11-27 16:20:47 +0200

    commented out logs

--- src/main/java/com/github/bruce_mig/spring_batch/config/StudentProcessor.java
+++ src/main/java/com/github/bruce_mig/spring_batch/config/StudentProcessor.java
@@ -13,7 +13,7 @@ public class StudentProcessor implements ItemProcessor<Student,Student> {
     public Student process(Student student) {
         // all the business logic goes here
         student.setId(null);
-        log.info("processing the item: {}", student);
+//        log.info("processing the item: {}", student);
         return student;
     }
 }
